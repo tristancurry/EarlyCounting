c4381735345c107f7167b7466b2f63cd80e9ecab
Tristan Miller <tristan.j.miller@gmail.com>
2022-05-02 15:01:25 +0930

    css layouty stuff

---
+++ web-edition/scripts/app.js
@@ -0,0 +1,11 @@
+const N_MAX = 10; // highest number expected
+
+//determine which style of grid to use, based on screen dimensions
+//1 - tall or wide: try for rows of 5 (anticipating 10 countables)
+//2 - not all that tall/wide: use a square grid whenever possible.
+
+//actually might be able to get CSS to handle cases matching 1: just set the width of
+//the countables appropriately and the grid should handle the rest?
+
+
+//
